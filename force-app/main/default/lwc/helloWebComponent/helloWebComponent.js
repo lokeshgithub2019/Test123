f44b113a5121dee0d78155b89876c7a459766a30
Lokesh Singh <lokeshs@positiveedge.com>
2020-01-08 12:41:19 +0530

    my first commmit

---
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -0,0 +1,17 @@
+import { LightningElement, track } from 'lwc';
+
+export default class HelloWebComponent extends LightningElement {
+
+    @track greeting  = "Trailblazer";
+    currentDate = new Date().toDateString();
+
+    get capitalizedGreeting()
+    {
+        return `Hello123 ${this.greeting.toUpperCase()}!`;
+    }
+
+    handleGreetingChange(event)
+    {
+        this.greeting = event.target.value;
+    }
+}
